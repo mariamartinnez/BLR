5bab69c9a768e209c4e0fb43e35670e973237cc7
María Martínez <mariamartinnez@gmail.com>
2020-08-25 16:20:45 +0200

    Add files via upload

--- demo_BLR.py
+++ demo_BLR.py
@@ -7,6 +7,7 @@ Created on Thu Jun  4 10:29:18 2020
 """
 
 import torch
+import numpy as np
 import random
 import matplotlib.pyplot as plt
 
@@ -16,11 +17,8 @@ from src.BayesianLogisticRegression import BayesianLogisticRegression
 #          TOY DATASET           #
 # ------------------------------ #
 
-N = 1000    # number of samples
+N = 500    # number of samples
 D = 50      # number of dimensions
-
-x = torch.zeros(N,D)
-y = torch.zeros(N)
 
 # Sample the weights from a Laplace distribution
 laplace = torch.distributions.laplace.Laplace(torch.tensor([0.0]), torch.tensor([1.0]))
@@ -35,12 +33,10 @@ w_true[13] = 0
 # 1. Sample the input data from a Normal distribution N(0,I).
 # 2. Compute the probability of y to be 1 following the LR model: sigmoid(x.T@w).
 # 3. Sample the label from a Bernoulli distribution using the obtained probability.
-for i in range(N):
-    x[i,:] = torch.FloatTensor(1,D).normal_()
-    y[i] = torch.sigmoid(x[i,:].view(1,-1)@w_true)
-    y[i] = torch.bernoulli(y[i])
-    
-y = y.unsqueeze(1)
+
+x = torch.bernoulli(torch.ones((N,D))*0.5)
+probs = torch.sigmoid(x@w_true)
+y = torch.bernoulli(probs)
 
 # ------------------------------ #
 #              BLR               #
@@ -51,14 +47,14 @@ input_dim = D
 hdim_mean = 100
 hdim_var = 100
 output_dim = D
-b = 0.7
+b = 1
 
 blr = BayesianLogisticRegression(input_dim, hdim_mean, hdim_var, output_dim, b)
 
 niter = 500     # number of SGD steps
-n_samples = 500 # number of samples used for each SGD step 
+n_samples = N # number of samples used for each SGD step 
 
-ELBO_vect = []
+ELBO_vect1 = []
 a = []
 bc = []
 
@@ -69,7 +65,7 @@ for i in range(niter):
     sampled_y = y[indice]
     
     blr.SGD_step(sampled_x, sampled_y, mc=200)
-    ELBO_vect.append(blr.ELBO_loss)  
+    ELBO_vect1.append(blr.ELBO_loss)  
     a.append(blr.a)
     bc.append(blr.bc)
 
@@ -80,7 +76,7 @@ std = torch.sqrt(torch.diag(blr.cov))
 # ------------------------------ #
 
 plt.figure(figsize = (10,5))
-plt.plot(ELBO_vect)
+plt.plot(ELBO_vect1)
 plt.xlabel('iter')
 plt.ylabel('ELBO')
 plt.title('Evolution of the ELBO loss')
@@ -93,17 +89,20 @@ plt.legend(loc='best')
 plt.title('Evolution of the ELBO terms')
 
 plt.figure(figsize = (10,5))
-plt.stem(w_true, label='w true')
-plt.stem(blr.mean.detach().numpy(), label='blr mean', linefmt='C1-', markerfmt='C1o')
+plt.stem(w_true, label='w true', basefmt = 'k')
+plt.stem(blr.mean.detach().numpy(), label='blr mean', linefmt='C1-', markerfmt='C1o', basefmt='k')
 plt.xlabel('feature')
 plt.ylabel('WEIGHT')
 plt.legend(loc='best')
 plt.title('True weights vs estimated weights')
 
+
 plt.figure(figsize = (10,5))
-plt.errorbar(range(D), w_true, linestyle='None', marker='o', label = 'w_true')
-plt.errorbar(range(D),blr.mean.detach().numpy(), std.detach().numpy(), linestyle='None', marker='o', label='blr estimation')
+plt.errorbar(range(D), w_true, linestyle='None', fmt='o', label = 'w_true')
+plt.errorbar(range(D),blr.mean.detach().numpy(), yerr = 0.5*std.detach().numpy(), linestyle='None', fmt='C1o', capsize=2, elinewidth=2, ecolor='red', label='blr estimation')
+plt.plot(range(D), np.zeros(D), color='k')
 plt.legend(loc='best')
 plt.xlabel('feature')
 plt.ylabel('WEIGHT')
 plt.title('True weights vs estimated weights')
+
